c02e6b9832274eed3b08ea7e73c2364fed039ff5
Douglas Henrique <dev.dhbarboza@gmail.com>
2024-02-01 17:51:21 -0300

    New: Ajusted in main component

--- interface/src/app/page.tsx
+++ interface/src/app/page.tsx
@@ -10,12 +10,14 @@ export default function Home() {
             <Header />
             <div className="flex flex-1 mx-auto  w-full h-full items-center justify-center">
                 <Container>
-                    <div className="max-w-[600px] w-full bg-secondary p-4 rounded-lg">
-                        <div>
-                            <span>Join</span>
-                            <span>Nwe Meeting</span>
+                    <div className="">
+                        <div className="flex items-center text-center">
+                            <span className="bg-secondary w-1/2  p-4 rounded-t-lg">
+                                Join
+                            </span>
+                            <span className="w-1/2">Nwe Meeting</span>
                         </div>
-                        <div className="space-y-4">
+                        <div className="min-w-[600px] w-full bg-secondary py-4 rounded-b-lg space-y-6 px-10">
                             <Input type="text" placeholder="s" />
                             <Input type="text" placeholder="s" />
                             <Button title="Enter" type="submit"></Button>
